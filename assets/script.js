72ec2e11c5bf7702d8e55c8747a66f85de037dd2
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-02-03 17:25:04 +0000

    added js file and input api key and url address

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// API key
+const APIKey = "d0736665d026d198ed2e5a4eedc5ac57";
+
+//the URL for querying the database
+const queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=" + APIKey;
+
+
+
