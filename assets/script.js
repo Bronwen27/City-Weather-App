f879d563aa13827e4831b5d02ddec39528b026f0
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-02-04 12:57:52 +0000

    updated js with api and url and query

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,27 @@
-// API key
-const APIKey = "d0736665d026d198ed2e5a4eedc5ac57";
+// onclick to trigger the Ajax call
+$("#form-input").on("click", function(event) {
 
-//the URL for querying the database
-const queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid=" + APIKey;
+event.preventDefault();
+
+// grab text from the input box
+var city = $("form-input").val();
+
+//API key
+var APIKey = "d0736665d026d198ed2e5a4eedc5ac57"
+
+//var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+
+var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $("#city-view").text(JSON.stringify(response));
+  });
+
+//------------------------------------------------------------
+})
 
 
 
