72b6cbf53395fc5b1783ed54cc235c2254c15b83
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-02-04 22:54:22 +0000

    linked api to search button and added bootstrap weather box

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,44 @@
 // onclick to trigger the Ajax call
-$("#search-form").on("click", function(event) {
+$("#search-button").on("click", function(event) {
 
 event.preventDefault();
-
 // grab text from the input box
-var city = $("#search-form").val();
+var city = $("#search-input").val().trim();
+// view city in console
+console.log(city)
 
 
 //API key
-var APIKey = "d0736665d026d198ed2e5a4eedc5ac57"
+let APIKey = "d0736665d026d198ed2e5a4eedc5ac57";
+// creating query
+let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+// search history 
+let searchHistory = []
+// search history section
+let searchHistorySection = $('#history')
+//search box form
+let searchForm = $('#search-form');
+//search text input
+let searchInput = $('#search-input');
 
-var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
-
+// calling query with ajax
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     $("#city-view").text(JSON.stringify(response));
-  });
+       if (searchHistory.push(search));
+        localStorage.setItem("search-history", JSON.stringify(search));
 
-  console.log("City value", city);
+      searchHistorySection.html("")
+    ;
+  });
+//console title city value:
+  console.log("City value:", city);
 //------------------------------------------------------------
 })
+
+
 
 
 
